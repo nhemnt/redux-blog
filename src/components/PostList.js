723c1e7b13035d30704cb4afc6c395c4e5acf7c8
hemant <nhemnt@gmail.com>
2019-01-07 00:11:31 +0530

    postList component

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostList extends React.Component { 
+    render() { 
+        return (
+            <div>
+                <h2>Post List</h2>
+            </div>
+        )
+    }
+}
+
+export default PostList;
